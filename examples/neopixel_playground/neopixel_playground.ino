88ae3a7a91aa26806f441a0194245a5d10412c21
Molly Nicholas <molecule1985@gmail.com>
2015-10-05 17:32:45 -0700

    Updated neopixel_playground.

--- examples/neopixel_playground/neopixel_playground.ino
+++ examples/neopixel_playground/neopixel_playground.ino
@@ -26,19 +26,13 @@ void setup() {
 
 void loop() {
   // To run a different function, change this method call to something else
-  rainbowCycle();
-  myNeopixelFunction(strip.numPixels(), turquoise);
+  turquoise_flash();
   // This just turns off all the LEDs
   chase(blue);
 }
 
-static void myNeopixelFunction(int numPixels, uint32_t c) {
- for(int i=0;i<numPixels;i++) {
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    strip.setPixelColor(i, c);
-    strip.show(); // This sends the updated pixel color to the hardware.
-    delay(delayVal); // Delay for a period of time (in milliseconds).
-  } 
+void turquoise_flash() {
+  cycle_color_flash(25, 51, 42);
 }
 
 /*
@@ -53,31 +47,25 @@ static void chase(uint32_t c) {
   }
 }
 
-// An equally distributed rainbow. Is there any other kind?
-void rainbowCycle() {
-  uint8_t wait = 5;
-  uint16_t i, j;
+void cycle_color_flash(int rStart, int gStart, int bStart) {
+  int Rstart = rStart;
+  int Gstart = gStart;
+  int Bstart = bStart;
 
-  for(j=0; j<256*2; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+
+  // setting the "end" values to 0 means the LEDs will fade
+  // too off. You can change these to other color values
+  // to fade between two colors.
+  int Rend, Gend, Bend = 0;
+  int n = 100;
+  for(int i = 0; i < n; i++) {// larger values of 'n' will give a smoother/slower transition.
+    for(int j = 0; j<strip.numPixels(); j++) {
+      float Rnew = Rstart + (Rend - Rstart) * i / n;
+      float Gnew = Gstart + (Gend - Gstart) * i / n;
+      float Bnew = Bstart + (Bend - Bstart) * i / n;
+      strip.setPixelColor(j, strip.Color(Rnew, Gnew, Bnew));
     }
-    strip.setBrightness(75); strip.show();
-    delay(wait);
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else if(WheelPos < 170) {
-    WheelPos -= 85;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  }
+    strip.show();
+    delay(10);
+  }  
 }
